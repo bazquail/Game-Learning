590561cc71b3ceb7a1c011f49cb14e4c7a61e63f
Elias <eliastopp@gmail.com>
2023-08-10 20:21:33 -0400

    feat: added virtual camera and walk anim

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ using UnityEngine.Rendering;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    bool isWalking;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -33,7 +34,14 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking() 
+    {
+        return isWalking;
     }
 }
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField] Player player;
+    const string IS_WALKING = "IsWalking";
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+}
